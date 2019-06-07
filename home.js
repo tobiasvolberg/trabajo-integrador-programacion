c7a7938f4542e76f8fefac59bbfe66ca1f73c84d
tobias <tvolberg@udesa.edu.ar>
2019-06-07 17:45:17 -0300

    Terminado el 1er carousel

--- home.js
+++ home.js
@@ -33,7 +33,7 @@ window.onload = function(){
     console.log(titulo);
 
     for (var i = 0; i < titulo.length; i++) {
-      document.querySelector("#carruselhome ul").innerHTML += '<li><img src="' +"https://image.tmdb.org/t/p/w500/" +titulo[i].poster_path +'" alt="" uk-cover><div class="uk-position-bottom uk-position-medium uk-text-center uk-light"><h3 class="uk-margin-remove">' + titulo[i].title + '</h3><p class="uk-margin-remove">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div></li>'
+      document.querySelector("#carruselhome ul").innerHTML += '<li><img src="' +"https://image.tmdb.org/t/p/w500/" +titulo[i].poster_path +'" alt="" uk-cover><div class="uk-position-bottom uk-position-medium uk-text-center uk-light"><h3 class="uk-margin-remove">' + titulo[i].title + '</h3><p class="uk-margin-remove">'+titulo[i].overview+'</p></div></li>'
     }
   })
   .catch(function(error){

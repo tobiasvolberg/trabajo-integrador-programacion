5e15550cd574901dec9c9c9070590dc12fe1cca4
jtabacman <jtabacman@gmail.com>
2019-06-14 18:43:34 -0300

    xsxs

--- home.js
+++ home.js
@@ -37,7 +37,7 @@ window.onload = function(){
       li = "<li>"
 
 
-      li += "<img src='https://image.tmdb.org/t/p/w500/" +titulo[i].poster_path +"'  uk-cover>"
+      li += "<img id='carrusel1' src='https://image.tmdb.org/t/p/w500/" +titulo[i].poster_path +"'  uk-cover>"
       li += "<div class='uk-position-bottom uk-position-medium uk-text-center uk-light'>"
       li += "<a href='detalleDePelicula.html?id="+titulo[i].id+"'>"
       li += "<h3 class='uk-margin-remove'>" + titulo[i].title + "</h3>"

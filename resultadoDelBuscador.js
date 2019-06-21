f69290e7c066173df6a3b09440436bf1aeb18399
tobias <tvolberg@udesa.edu.ar>
2019-06-20 21:08:55 -0300

    Hice lo de buscar y si no encuentra que tire mensaje

--- resultadoDelBuscador.js
+++ resultadoDelBuscador.js
@@ -39,6 +39,15 @@ window.addEventListener("load",function(){
   .then(function(info) {
   var titulo = info.results
   console.log(titulo);
+  if (titulo.length == 0) {
+    // alert("no")
+    UIkit.notification({
+      message: 'No se encontro lo buscado',
+      status: 'warning',
+      pos: 'top-center',
+      timeout: 2000,
+    })
+  }
 
   for (var i = 0; i < titulo.length; i++) {
     li = "<li>"
@@ -54,7 +63,7 @@ window.addEventListener("load",function(){
     li += "</li>"
 
     document.querySelector("#carruselhome ul").innerHTML += li
-    
+
     // document.querySelector("#carruselhome ul").innerHTML += '<li><img src="' +"https://image.tmdb.org/t/p/w500" +titulo[i].poster_path +'" alt="" uk-cover><div class="uk-position-bottom uk-position-medium uk-text-center uk-light"><h3 class="uk-margin-remove">' + titulo[i].title + '</h3><p class="uk-margin-remove">'+titulo[i].overview+'</p></div></li>'
   }
 
